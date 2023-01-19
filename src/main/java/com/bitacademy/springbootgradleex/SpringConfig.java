cb09ccc01f7dc7a8724b3eaa702d51a3502eaa71
suntaehun <sth7862@naver.com>
2023-01-19 15:30:44 +0900

    jpa

--- src/main/java/com/bitacademy/springbootgradleex/SpringConfig.java
+++ src/main/java/com/bitacademy/springbootgradleex/SpringConfig.java
@@ -1,33 +1,38 @@
 package com.bitacademy.springbootgradleex;
 
-import javax.persistence.EntityManager;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.bitacademy.springbootgradleex.repository.JpaMemberRepository;
+import com.bitacademy.springbootgradleex.aop.TimeTraceAop;
 import com.bitacademy.springbootgradleex.repository.MemberRepository;
 import com.bitacademy.springbootgradleex.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 	
-	private EntityManager em;
+	private final MemberRepository memberRepository;
 	
 	@Autowired
-	public SpringConfig(EntityManager em) {
-		this.em = em;
+	public SpringConfig(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
 	}
 	
 	@Bean
 	public MemberService memberService() {
-		return new MemberService(memberRepository());
+		return new MemberService(memberRepository);
 	}
 	
-	@Bean
-	public MemberRepository memberRepository(){
+//	TimeTraceAop에 Component로 대체가능
+//	@Bean
+//	public TimeTraceAop timeTraceAop() {
+//		return new TimeTraceAop();
+//	}
+	
+//	@Bean
+//	public MemberRepository memberRepository(){
 //		return new MemoryMemberRepository();
-		return new JpaMemberRepository(em);
-	}
+//		return new JdbcTemplateMemberRepository(dataSource);
+//		return new JpaMemberRepository(em);
+//	}
 }
